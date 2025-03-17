e86f4f656f8358b659ccdd75a7183f04dc9713dd
Carlos Denil <carlosdenilss@gmail.com>
2025-03-17 15:34:29 -0300

    fix: corrige ordem de registro para garantir inicialização correta das dependências, registrando o serviços de infraestrutura antes da camada de aplicação

--- ProjetoBiblioteca.API/Program.cs
+++ ProjetoBiblioteca.API/Program.cs
@@ -5,35 +5,32 @@ using ProjetoBiblioteca.Infrastructure;
 using ProjetoBiblioteca.Infrastructure.Serialization.ProjetoBiblioteca.Infrastructure.Serialization;
 using Microsoft.OpenApi.Models;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddExceptionHandler<ApiExceptionHandler>();
 builder.Services.AddProblemDetails();
 builder.Services.AddMemoryCache();
 
-builder.Services
-    .AddApplication()
-    .AddInfrastrucutre(builder.Configuration);
-builder.Services.AddControllers().AddJsonOptions(options =>
+
+builder.Services.AddInfrastrucutre(builder.Configuration);
+
+
+builder.Services.AddApplication();
+
+
+builder.Services.AddControllers().AddJsonOptions(options => 
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     options.JsonSerializerOptions.Converters.Add(new DateFormatJsonConverter());
-
 });
-
 
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjetoBiblioteca API", Version = "v1" });
 
-    // Definição da segurança JWT no Swagger
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Name = "Authorization",
@@ -44,7 +41,7 @@ builder.Services.AddSwaggerGen(c =>
         Description = "Insira o token JWT no formato: Bearer {seu_token}"
     });
 
-    // Configuração do Swagger para exigir autenticação
+    
     c.AddSecurityRequirement(new OpenApiSecurityRequirement
     {
         {
@@ -64,7 +61,7 @@ builder.Services.AddSwaggerGen(c =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
